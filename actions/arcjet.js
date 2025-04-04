ffc752d1bf2282b63d963d3337a8157ffd9649c3
vikram17dp <vikramdp505@gmail.com>
2025-04-04 18:59:57 +0530

    setting up rate limiting with arcjet

---
+++ actions/arcjet.js
@@ -0,0 +1,14 @@
+import arcjet, { tokenBucket } from "@arcjet/next";
+
+export const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  characteristics: ["ip.src"], // track requests by IP address
+  rules: [
+    tokenBucket({
+      mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
+      refillRate: 10, // refill 10 tokens per interval
+      interval: 3600, //per hour
+      capacity: 100, // bucket maximum capacity of 100 tokens
+    }),
+  ],
+});
