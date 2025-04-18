d72896a4e0ce147317215fd2a91030bb991d191f
vikram17dp <vikramdp505@gmail.com>
2025-04-18 18:23:41 +0530

    adding shiled and bot protection using an arcjet

--- middleware.js
+++ middleware.js
@@ -1,3 +1,4 @@
+import arcjet, { createMiddleware, detectBot, shield } from "@arcjet/next";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedUser = createRouteMatcher([
@@ -6,14 +7,35 @@ const isProtectedUser = createRouteMatcher([
     "/reservations(.*)"
 ])
 
-export default clerkMiddleware(async(auth,req)=>{
+
+// Create Arcjet middleware
+const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  // characteristics: ["userId"], // Track based on Clerk userId
+  rules: [
+    // Shield protection for content and security
+    shield({
+      mode: "LIVE",
+    }),
+    detectBot({
+      mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
+      allow: [
+        "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
+        // See the full list at https://arcjet.com/bot-list
+      ],
+    }),
+  ],
+});
+
+const clerk =  clerkMiddleware(async(auth,req)=>{
     const {userId} = await auth();
     if(!userId && isProtectedUser(req)){
         const {redirectToSignin} = await auth();
         return redirectToSignin();
     }
 });
-
+// Chain middlewares - ArcJet runs first, then Clerk
+export default createMiddleware(aj, clerk);
 export const config = {
   matcher: [
 
